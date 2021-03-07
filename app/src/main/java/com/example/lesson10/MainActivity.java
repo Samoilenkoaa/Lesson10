8b4ac0bd7758ae71b2e80a41442ee5d12d72c1d5
geekbrains <samoilenkoaa1990@mail.ru>
2021-03-07 20:08:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lesson10/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lesson10;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Overrde
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawer_activity);
+
+    }
+
+
+}
